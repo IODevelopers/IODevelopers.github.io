4ff1dbebf68bb390837df0e29b9a8c07d77f784f
Rajat Jaic Mendus <jaicmendus@gmail.com>
2019-03-16 09:45:54 +0530

    Dynamic user data

--- js/load-values.js
+++ js/load-values.js
@@ -1,11 +1,36 @@
 $(document).ready(function() {
-    // all custom jQuery will go here
-    $.getJSON("https://9nvv7wpamb.execute-api.ap-southeast-1.amazonaws.com/Development/get-leaderboard", function(data){
 
-            $("#ads").append(data[0]['AdsViewed']);
-            $("#moneygen").append("₹"+ data[0]['MoneyGenerated']);
-            $("#moneygain").append("₹"+ data[0]['MoneyGained']);
-            $("#moneydon").append("₹"+ data[0]['MoneyDonated']);
-    });
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyDHmBeUmbW8PHQEAXarTmtMZfw3pboGShc",
+      authDomain: "karnakodekochi.firebaseapp.com",
+      databaseURL: "https://karnakodekochi.firebaseio.com",
+      projectId: "karnakodekochi",
+      storageBucket: "karnakodekochi.appspot.com",
+      messagingSenderId: "791941464258"
+    };
+    firebase.initializeApp(config);
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+
+		    var xhr = new XMLHttpRequest();
+			url = "https://9nvv7wpamb.execute-api.ap-southeast-1.amazonaws.com/Development/get-userdetails";
+			xhr.open("POST", url, true);
+			xhr.setRequestHeader("Content-Type", "application/json");
+			xhr.onreadystatechange = function () {
+		    if (xhr.readyState === 4 && xhr.status === 200) {
+		        var json = JSON.parse(xhr.responseText);
+		        console.log(json["Data"]);
+		        $("#ads").append(json["Data"]['AdsViewed']);
+		        $("#moneygen").append("₹"+ json["Data"]['MoneyGenerated']);
+		        $("#moneygain").append("₹"+ json["Data"]['MoneyGained']);
+		        $("#moneydon").append("₹"+ json["Data"]['MoneyDonated']);
+		    	}
+			};
+			var data = JSON.stringify({"Username": user.uid});
+			xhr.send(data);
+		}
+	});
       
 });
