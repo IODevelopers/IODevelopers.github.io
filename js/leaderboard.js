f43e0c1138a6c045c555a5353a82b5664f02ca5f
Philson K Philip <philson.k.philip@gmail.com>
2019-03-16 10:52:40 +0530

    leaderboard displayed

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $.getJSON("https://9nvv7wpamb.execute-api.ap-southeast-1.amazonaws.com/Development/get-leaderboard", function(data){
         for (var i = 0, len = data.length; i < len; i++) {
             console.log(data[i]);
-            $("#addtbody").append("<tr><td>"+data[i]['Rank']+"</td><td>"+data[i]['DisplayName']+"</td><td>"+data[i]['AdsViewed']+"</td><td>"+data[i]['MoneyGenerated']+"</td><td>"+data[i]['MoneyDonated']+"</td><td>"+data[i]['MoneyGained']+"</td></tr>");
+            $("#addtbody").append("<tr><td>"+data[i]['Rank']+"</td><td>"+data[i]['DisplayName']+"</td><td>"+data[i]['AdsViewed']+"</td><td>"+data[i]['MoneyGenerated']+"</td><td>"+data[i]['MoneyDonated']+"</td><td>"+data[i]['MoneyGained']+"</td><td>"+data[i]['Percentage']+"</td></tr>");
         }
     });
       
